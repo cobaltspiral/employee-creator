70314a48dcd84cbf314d05bcf0c17930765fae59
cobaltspiral <kikola93@hotmail.it>
2026-09-06 15:23:14 +0100

    Add Employee fields, getters and setters

--- src/main/java/com/cobaltspiral/employee_creator/employee/entity/Employee.java
+++ src/main/java/com/cobaltspiral/employee_creator/employee/entity/Employee.java
@@ -1,5 +1,142 @@
 package com.cobaltspiral.employee_creator.employee.entity;
 
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "employees")
 public class Employee {
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String middleName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String email;
+
+    @Column
+    private String mobileNumber;
+
+    @Column
+    private String address;
+
+    @Column
+    private String contractType;
+
+    @Column
+    private Date startDate;
+
+    @Column
+    private Date endDate;
+
+    @Column
+    private String partTimeOrFullTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getContractType() {
+        return contractType;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public String getPartTimeOrFullTime() {
+        return partTimeOrFullTime;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setContractType(String contractType) {
+        this.contractType = contractType;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public void setPartTimeOrFullTime(String partTimeOrFullTime) {
+        this.partTimeOrFullTime = partTimeOrFullTime;
+    }
+
+    
+    
+
 
 }
