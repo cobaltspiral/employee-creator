b4ac5d1b6b7f9f6fe89c15cdce4fcb03012d0f06
cobaltspiral <kikola93@hotmail.it>
2026-09-09 17:15:17 +0100

    Add Contract entity

---
+++ src/main/java/com/cobaltspiral/employee_creator/contract/ContractRepository.java
@@ -0,0 +1,5 @@
+package com.cobaltspiral.employee_creator.contract;
+
+public class ContractRepository {
+    
+}
---
+++ src/main/java/com/cobaltspiral/employee_creator/contract/entity/Contract.java
@@ -0,0 +1,5 @@
+package com.cobaltspiral.employee_creator.contract.entity;
+
+public class Contract {
+    
+}
