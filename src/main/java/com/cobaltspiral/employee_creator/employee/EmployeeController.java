bfe37a357ae3b881d572c24d508da81c14e33003
cobaltspiral <kikola93@hotmail.it>
2026-09-09 17:32:55 +0100

    Configure Employee Controller

--- src/main/java/com/cobaltspiral/employee_creator/employee/EmployeeController.java
+++ src/main/java/com/cobaltspiral/employee_creator/employee/EmployeeController.java
@@ -1,5 +1,19 @@
 package com.cobaltspiral.employee_creator.employee;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+
+@RestController
+@RequestMapping ("/employees")
+@Tag (name = "Employees", description = "Employees endpoint")
 public class EmployeeController {
+    
+    private final EmployeeService employeeService;
+    
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
     
 }
